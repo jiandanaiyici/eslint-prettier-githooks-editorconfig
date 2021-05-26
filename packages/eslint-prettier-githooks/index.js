9106dc5de6b387450190ededb86cfd75b4331209
聂海锋 <niexiaofei@yahoo.com>
2021-05-27 02:52:03 +0800

    feat: 添加 husky

---
+++ packages/eslint-prettier-githooks/index.js
@@ -0,0 +1,7 @@
+const obj = {
+  "a": 1,
+  "b": 'b',
+  "c": 'd',
+  "address": 'China',
+  "age": 1
+};
---
+++ packages/eslint-prettier-githooks/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  parser: 'typescript'
+};
